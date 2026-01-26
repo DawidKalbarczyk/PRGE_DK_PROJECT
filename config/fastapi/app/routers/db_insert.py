043ccd8317fdd37bf42d0727a4620c17a31373d3
Dawid <u56128@student.wat.edu.pl>
2026-01-27 00:57:15 +0100

    feat: added copying from initial container to the next one

--- config/fastapi/app/routers/db_insert.py
+++ config/fastapi/app/routers/db_insert.py
@@ -5,6 +5,7 @@ router_insert = APIRouter()
 
 from app.settings import db_name, db_user, db_password
 
+
 def connect_to_db(db_name: str, db_user: str, db_password: str):
     return create_engine(
         f"postgresql://{db_user}:{db_password}@postgis:5432/{db_name}"
